64eb124465ceae4021040556fcac49490ebc1855
suheb <suhebism@gmail.com>
2024-09-24 21:06:59 +0530

    11try

--- app/all-quiz/page.jsx
+++ app/all-quiz/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
       <div className="flex items-center w-full gap-5">
         <Link href="/" className="absolute top-0 left-5">Back</Link>
       </div>
-        <h1 className="text-center">All Quiz</h1>
+        <h1 className="text-center">All Quizs</h1>
     </div>
   );
 };
