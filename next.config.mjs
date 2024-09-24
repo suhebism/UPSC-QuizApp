6f5a1d916c39f669b627622d5fbb564f157d2d87
suheb <suhebism@gmail.com>
2024-09-24 23:26:38 +0530

    25 try

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,3 @@
-// import withPWA from 'next-pwa';
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output: 'export',  // For static site generation
-//   images: {
-//     unoptimized: true,  // Disable image optimization for static export
-//     domains: ['images.unsplash.com'],
-//   },
-// };
-
-// export default withPWA({
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV !== 'production',  // Enable PWA only in production
-// })(nextConfig);
-
-
 import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
@@ -27,13 +7,9 @@ const nextConfig = {
     unoptimized: true, // Disable image optimization for static export
     domains: ['images.unsplash.com'],
   },
-  // Here is where you should set the export configuration
-  // Ensure to use this only if you are exporting for static site generation
-  // output: 'export', // This is for next 13 or above with the new app directory
 };
 
 export default withPWA({
-  dest: 'public', // PWA files destination
   register: true, // Register service worker
   skipWaiting: true, // Skip waiting for service worker activation
   disable: process.env.NODE_ENV !== 'production', // Enable PWA only in production
