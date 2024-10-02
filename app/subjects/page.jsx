6aadcd716d37cf41cc9804c12f706427aa2992fa
suheb <suhebism@gmail.com>
2024-10-03 04:26:22 +0530

    2nd commit

--- app/subjects/page.jsx
+++ app/subjects/page.jsx
@@ -1,12 +1,12 @@
 import TopicCard from '@/components/TopicCard';
 import React from 'react';
-import quizData from '../../data/quizData.js'
-import Subjects from '@/components/Subjects';
+// import quizData from '../../data/quizData.js'
+// import Subjects from '@/components/Subjects';
 
 const page =()=>{
     return(
         <div>
-            <Subjects subjects={quizData.subjects} />
+            {/* <Subjects subjects={quizData.subjects} /> */}
             
         </div>
     )
--- components/Subjects.jsx
+++ components/Subjects.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Subjects = ({subjects}) => {
   return (
     <div>
-      {subjects.map((subject)=>{
+      {/* {subjects.map((subject)=>{
         <div key={subject.name}>{subject.name}</div>
-      })}
+      })} */}
     </div>
   )
 }
